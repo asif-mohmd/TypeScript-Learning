4fa671f4d3082c560dbb588fa1bbf99b79488248
asif-mohmd <asifasifpsps@gmail.com>
2024-03-06 19:34:25 +0530

    Types defined

---
+++ index.js
@@ -0,0 +1,10 @@
+// infer types (Implcit types)
+var userName = "Tyescript 1";
+// userName = 0
+// Explicit types : means we can mension the type 
+var userName2 = "Type Script 2";
+var number = 3000;
+var isSubscribed = true;
+var skills = ["coding", "typing", "reading"];
+var skills2 = [1, 2, 3, 4, 5, 6];
+var userDetails = { name: "asif", age: 21 };
--- index.ts
+++ index.ts
@@ -1,3 +1,15 @@
-let userName = "Debug media"
+// infer types (Implcit types)
+let userName = "Tyescript 1"
 
-userName = 0
+// userName = 0
+
+
+// Explicit types : means we can mension the type 
+
+let userName2:string = "Type Script 2"
+let number: number = 3000
+let isSubscribed : boolean = true
+let skills: string[] = ["coding","typing","reading"]
+let skills2: number[] = [1,2,3,4,5,6]
+let userDetails:{name:string,age:number} = {name:"asif",age:21}
+
