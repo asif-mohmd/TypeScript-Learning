b20bdab793ec32f6d0fc72da90488c15aaf205ee
asif-mohmd <asifasifpsps@gmail.com>
2024-03-06 21:12:47 +0530

    functions

--- index.ts
+++ index.ts
@@ -5,11 +5,16 @@ let userName = "Tyescript 1"
 
 
 // Explicit types : means we can mension the type 
+// always try to mension type . dont depend too much on infer
 
 let userName2:string = "Type Script 2"
 let number: number = 3000
 let isSubscribed : boolean = true
 let skills: string[] = ["coding","typing","reading"]
 let skills2: number[] = [1,2,3,4,5,6]
+
 let userDetails:{name:string,age:number} = {name:"asif",age:21}
 
+
+// union operator
+let skill3: (string|number)[] = ["coding","typing",2,3,"reading"]
--- interface.ts
+++ interface.ts
@@ -3,12 +3,50 @@
 
 interface Details {
     name:string;
-    age:number;
-    salary:number
+    // optional checking . but its check the type . if type is incorrect it will shows error
+    age?:number;
+    // union operator
+    salary:number | string
+}
+
+interface Seller {
+    name:string;
+    // optional checking . but its check the type . if type is incorrect it will shows error
+    age?:number;
+    // union operator
+    salary:number | string;
+    getInfo?:()=>void;
 }
 
 let adminDetails: Details= {
     name:"asif",
     age:21,
-    salary:7000
+    salary: "7000"
 }
+
+let sellerDetails: Seller= {
+    name:"jishnu",
+    age:26,
+    salary: "9000",
+
+    }
+
+
+
+function getUserName(adminDetails:Details):string {
+    return adminDetails.name
+
+}
+getUserName(adminDetails)
+
+
+function getAdminDetails({name,age}:{name:string,age:number}):number {
+    return age
+}
+getAdminDetails({name:"jacky",age:25})
+
+function getSellerDetails({name,age}:{name:string,age:number}):void {
+// if no return we can add void data type
+    console.log("seller")
+}
+getAdminDetails({name:"jacky",age:25})
